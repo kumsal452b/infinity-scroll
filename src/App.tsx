0c89e124bfa3e19cb209a03c2d04afd0745d4724
a <yahya.alatas@outlook.com>
2024-02-21 18:57:57 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { fetchItems } from "./api/items";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { useInView } from "react-intersection-observer";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const { data, error, status, fetchNextPage, isFetchingNextPage } =
+    useInfiniteQuery({
+      queryKey: ["items"],
+      queryFn: fetchItems,
+      initialPageParam: 0,
+      getNextPageParam: (lastPage: any) => lastPage.nextPage,
+    });
+  const { ref, inView } = useInView();
+  useEffect(() => {
+    if (inView) {
+      fetchNextPage();
+    }
+    return () => {};
+  }, [fetchNextPage, inView]);
+
+  return status === "pending" ? (
+    <div>Loading...</div>
+  ) : status === "error" ? (
+    <div>Error: {error.message}</div>
+  ) : (
+    <div className="flex flex-col gap-2">
+      {data?.pages.map((page, i) => {
+        return (
+          <div className="flex flex-col gap-2" key={page.currentPage}>
+            {page.data.map((item: any) => {
+              return (
+                <div
+                  className="bg-slate-400 rounded-md bg-grayscale-700 p-10"
+                  key={item.id}
+                >
+                  {item.name}
+                </div>
+              );
+            })}
+          </div>
+        );
+      })}
+      <div ref={ref}>{isFetchingNextPage && "Loading..."}</div>
     </div>
   );
 }
